9aed4842c110d2b7874752801f6b2a0f7519ec78
organizedanvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-03-30 21:42:01 -0400

    Update #2: Added code for .txt file read and write to store tasks across instances

--- todoapp.py
+++ todoapp.py
@@ -1,45 +1,49 @@
-actions = []
-
-print("")
-print("Welcome to the infinite list grower!")
-print("")
-print("Follow the prompt to add or show the list!")
-print("")
+print("Welcome to your ToDo App!\n")
+print("Follow the prompt below to begin.\n")
 
 while True:
-    user_action = input("Type add, show, edit, complete, or exit: ")
-    print("")
+    user_action = input("\nType add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            action = input("Enter what you would like to add to the list: ")
-            print("")
+            print("\nAdd a ToDo to your tasklist!")
+            action = input("Enter your task: ") + "\n"
             action = action.title()
+
+            file = open('todo.txt', 'r')
+            actions = file.readlines()
+            file.close()
+
             actions.append(action)
+
+            file = open('todo.txt', 'w')
+            file.writelines(actions)
+            file.close()
         case 'show':
+            print("\nHere's your current list of tasks:\n")
+
+            file = open('todo.txt', 'r')
+            actions = file.readlines()
+            file.close()
+
+            #new_actions = [item.strip('\n') for item in actions]
+
             for i, value in enumerate(actions, 1):
-            print("{}. {}".format(i, value))
-            print("")
+                print("{}. {}".format(i, value.strip('\n')))
         case 'edit':
-            number = int(input("Enter the number of the item you would like to edit: "))
-            print("")
+            number = int(input("\nEnter the number of the task you would like to edit: "))
             number -= 1
-            new_action = input("Enter a new value: ")
-            print("")
+            new_action = input("\nEnter a new value: ")
             new_action = new_action.title()
             actions[number] = new_action
         case 'complete':
-            print("")
-            comp = int(input("Please enter the number of the item to complete: "))
-            print("")
+            comp = int(input("\nPlease enter the number of the task you would like to mark as complete: "))
             comp -= 1
             actions.pop(comp)
         case 'exit':
             break
         case _:
-            print("This command is unknown! Please try again")
-            print("")
+            print("\nThis command is unknown! Please try again\n")
 
-print("")
-print("Goodbye!")
+print("\nGoodbye!")
