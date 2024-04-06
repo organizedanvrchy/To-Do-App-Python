2eb5678667c93812831e40f04474a70f83983b6e
organizedanvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-04-06 16:20:45 -0400

    Update #3: Upgraded read/write functions & Added read/write function to .txt file for show/complete/edit

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,5 @@
-print("Welcome to your ToDo App!\n")
-print("Follow the prompt below to begin.\n")
+print("Welcome to your ToDo App!")
+print("Follow the prompt below to begin.")
 
 while True:
     user_action = input("\nType add, show, edit, complete, or exit: ")
@@ -7,42 +7,58 @@ while True:
 
     match user_action:
         case 'add':
-            print("\nAdd a ToDo to your tasklist!")
+            print("\nAdd a task to your list!")
             action = input("Enter your task: ") + "\n"
             action = action.title()
 
-            file = open('todo.txt', 'r')
-            actions = file.readlines()
-            file.close()
+            with open('todo.txt', 'r') as file:
+                actions = file.readlines()
 
             actions.append(action)
 
-            file = open('todo.txt', 'w')
-            file.writelines(actions)
-            file.close()
+            with open('todo.txt', 'w') as file:
+                file.writelines(actions)
+
         case 'show':
             print("\nHere's your current list of tasks:\n")
 
-            file = open('todo.txt', 'r')
-            actions = file.readlines()
-            file.close()
+            with open('todo.txt', 'r') as file:
+                actions = file.readlines()
 
             #new_actions = [item.strip('\n') for item in actions]
 
             for i, value in enumerate(actions, 1):
                 print("{}. {}".format(i, value.strip('\n')))
+
         case 'edit':
             number = int(input("\nEnter the number of the task you would like to edit: "))
             number -= 1
+
+            with open('todo.txt', 'r') as file:
+                actions = file.readlines()
+            
             new_action = input("\nEnter a new value: ")
-            new_action = new_action.title()
-            actions[number] = new_action
+            actions[number] = new_action.title() + '\n'
+
+            with open('todo.txt', 'w') as file:
+                file.writelines(actions)
+
         case 'complete':
             comp = int(input("\nPlease enter the number of the task you would like to mark as complete: "))
-            comp -= 1
-            actions.pop(comp)
+            
+            with open('todo.txt', 'r') as file:
+                actions = file.readlines()
+
+            tmp = comp - 1
+            action_remove = actions[tmp]
+            actions.pop(tmp)
+
+            with open('todo.txt', 'w') as file:
+                file.writelines(actions)
+
         case 'exit':
             break
+
         case _:
             print("\nThis command is unknown! Please try again\n")
 
