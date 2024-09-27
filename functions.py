5bad4ad7e93cb9c9a11f8b656d6bed93923537da
Anvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-09-27 01:22:18 -0400

    Update functions.py
    added FILEPATH variable to make file path more easily adjustable

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
-def getTasks(filepath='task.txt'):
+FILEPATH = "task.txt"
+
+def getTasks(filepath=FILEPATH):
     """ Reads text file and return list of task items"""
     with open(filepath, 'r') as file:
         a = file.readlines()
     return a
 
-def addTasks(a, filepath='task.txt'):
+def addTasks(a, filepath=FILEPATH):
     """ Writes user provided task to list of task items"""
     with open('task.txt', 'w') as file:
         file.writelines(a)
